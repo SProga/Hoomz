d92843ebd46dae882399dda7b13ac1ca9c70faa1
Proga147 <shane.306@hotmail.com>
2022-02-27 17:01:15 -0400

    added rest features initial

---
+++ themes/hoomz-theme/includes/search_hoomz.php
@@ -0,0 +1,42 @@
+<?php
+
+
+add_action('rest_api_init' , 'hoomzRegisterSearch');
+
+function hoomzRegisterSearch() {
+    register_rest_route('hoomz/v1','search', array(
+        'methods' => WP_REST_SERVER::READABLE,
+        'callback' => 'hoomzSearchResults'
+    ));
+}
+
+function hoomzSearchResults($data) {
+    $Queries = new WP_Query(array(
+        'post_type' => array('post','page','hoom'),
+        's' => sanitize_text_field($data['term'])
+    ));
+    
+    $results = array(
+        'hoomz' => array(),
+    );
+    
+    while($Queries->have_posts()) {
+        $Queries->the_post();
+        if(get_post_type() == 'hoom') {
+            array_push($results['hoomz'],array(
+                "title" => get_the_title(),
+                "permalink" => get_the_permalink(),
+                "hoomz_image" => get_field('hoomz_image'),
+                "hoomz_bedrooms" => get_field('hoomz_bedrooms'),
+                "hoomz_bathrooms"=> get_field('hoomz_bathrooms'),
+                "hoomz_sqfeet"=> get_field('hoomz_sqfeet'),
+                "hoomz_pool" => get_field('hoomz_pool'),
+                "postType" => get_post_type(),
+                "hoomz_location"=> get_post(),
+                "authorName" => get_the_author()
+            ));
+        }   
+    };
+
+    return $results;
+}
--- themes/hoomz-theme/js/script.js
+++ themes/hoomz-theme/js/script.js
@@ -1,27 +1,57 @@
-const tabs = document.querySelector(".wrapper");
-const tabButton = document.querySelectorAll(".tab-button");
-const contents = document.querySelectorAll(".content");
+jQuery(document).ready(function ($) {
+	const tabs = document.querySelector(".wrapper");
+	const tabButton = document.querySelectorAll(".tab-button");
+	const contents = document.querySelectorAll(".content");
 
-tabs.onclick = (e) => {
-	const id = e.target.dataset.id;
+	if (tabs) {
+		tabs.onclick = (e) => {
+			const id = e.target.dataset.id;
 
-	if (id) {
-		// if (id !== "map") {
-		// 	let themap = document.querySelector(".themap");
-		// 	themap.style.cssText = `display:none`;
-		// } else {
-		// 	let themap = document.querySelector(".themap");
-		// 	themap.style.cssText = `display:block`;
-		// }
-		tabButton.forEach((btn) => {
-			btn.classList.remove("active");
-		});
-		e.target.classList.add("active");
+			if (id) {
+				// if (id !== "map") {
+				// 	let themap = document.querySelector(".themap");
+				// 	themap.style.cssText = `display:none`;
+				// } else {
+				// 	let themap = document.querySelector(".themap");
+				// 	themap.style.cssText = `display:block`;
+				// }
+				tabButton.forEach((btn) => {
+					btn.classList.remove("active");
+				});
+				e.target.classList.add("active");
 
-		contents.forEach((content) => {
-			content.classList.remove("active");
-		});
-		const element = document.getElementById(id);
-		element.classList.add("active");
+				contents.forEach((content) => {
+					content.classList.remove("active");
+				});
+				const element = document.getElementById(id);
+				element.classList.add("active");
+			}
+		};
 	}
-};
+
+	let btn = $(".btn--submit").on("click", getInputs);
+
+	function getInputs(e) {
+		let type = $("[name='type']").val();
+		let category = $("[name='category'").val();
+		let price = $("[name='price']").val();
+		let location = $("[name='location'").val();
+		let data = {
+			type,
+			category,
+			price,
+			location,
+		};
+
+		let url = hoomzData.root_url + "/wp-json/hoomz/v1/search";
+
+		fetch(url, {
+			method: "GET",
+		})
+			.then((res) => res.json())
+			.then((res) => console.log(res));
+		// e.preventDefault();
+		// let form = $(".form__filter");
+		// console.dir(form);
+	}
+});
